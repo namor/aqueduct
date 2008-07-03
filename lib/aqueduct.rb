5a3de58cfebe728d3d0d44ffbe657113d8a9bb98
John Wong <john@flexnode.com>
2008-07-03 17:09:14 +0800

    Move attribute settings above before_filter

--- lib/aqueduct.rb
+++ lib/aqueduct.rb
@@ -6,35 +6,33 @@ module Aqueduct
   module ClassMethods
     def html_sanitizer(*attr_names)
       options = attr_names.last.is_a?(Hash) ? attr_names.pop : {}
-      attr_names.each { |attr_name| setup_callbacks_for(:html, attr_name) }
-
       write_inheritable_attribute(:aqueduct_html_options, options)
       class_inheritable_reader :aqueduct_html_options
-      
+
+      attr_names.each { |attr_name| setup_callbacks_for(:html, attr_name) }
+
     end
     
     def css_sanitizer(*attr_names)
       options = attr_names.last.is_a?(Hash) ? attr_names.pop : {}
-      attr_names.each { |attr_name| setup_callbacks_for(:css, attr_name) }
-
       write_inheritable_attribute(:aqueduct_css_options, options)
       class_inheritable_reader :aqueduct_css_options
-      
+
+      attr_names.each { |attr_name| setup_callbacks_for(:css, attr_name) }
     end
     
     def embed_sanitizer(*attr_names)
       options = attr_names.last.is_a?(Hash) ? attr_names.pop : {}
-      attr_names.each { |attr_name| setup_callbacks_for(:embed, attr_name) }
-
       write_inheritable_attribute(:aqueduct_embed_options, options)
       class_inheritable_reader :aqueduct_embed_options
-      
+
+      attr_names.each { |attr_name| setup_callbacks_for(:embed, attr_name) }
     end
     
     def setup_callbacks_for(processor, attr_name, options = {})
       case processor
       when :html
-        parser = Aqueduct::HTML.new(aqueduct_html_options)
+        parser = Aqueduct::HTML.new( aqueduct_html_options )
       when :css
         parser = Aqueduct::CSS.new(aqueduct_css_options)
       when :embed
